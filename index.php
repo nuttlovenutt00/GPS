40929072ffc8103bb12a2bc43a25b0166e67488a
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-02-26 23:06:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
         <script type="text/javascript" src="https://api.longdo.com/map/?key=05431d206b12178770f382c7a50f50a1 "></script>
         <script>
 
-
           function init() {
             map = new longdo.Map({
               placeholder: document.getElementById('map')
@@ -16,8 +15,13 @@
             map.location(longdo.LocationMode.Geolocation);
             var result = map.location(); 
             console.log(result);
-            document.getElementById('gps').value=result['lat']+","+result['lon'];
+            document.getElementById('showgps').value=result['lat']+","+result['lon'];
           }
         </script>
-
+    </head>
+    <body onload="init();">
+        <div id="map" style="display:none"></div>
+        <button onclick="ghk()">Geolocation</button>
+        <input type="text" value="" id="show">
+    </body>
 
