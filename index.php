bb636bf99c0a3f9d25ba1d866fb2bb756f940542
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-02-29 15:00:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
+<!DOCTYPE HTML>
+  <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Map Location Sample | Longdo Map</title>
+        <style type="text/css">
+          html{
+            height:100%; 
+          }
+          body{ 
+            margin:0px;
+            height:100%; 
+          }
+          #map {
+            height: 100%;
+          }
+        </style>
 
-
-        <script type="text/javascript" src="https://api.longdo.com/map/?key=05431d206b12178770f382c7a50f50a1 "></script>
+        <script type="text/javascript" src="https://api.longdo.com/map/?key=[YOUR_KEY_API]"></script>
         <script>
-
           function init() {
             map = new longdo.Map({
               placeholder: document.getElementById('map')
             });
-          }
-         function ghk(){
-          map.location(longdo.LocationMode.Geolocation);
-          var result = map.location();
-          console.log(result);
-            document.getElementById('gps1').value=result['lat']+","+result['lon'];
-          
+            map.location({ lon:100, lat:16 }, true); // go to 100, 16 when created map
           }
         </script>
     </head>
     <body onload="init();">
-      <input type="text" name="gps1" id="gps1" value="">
-      <button onclick="ghk()">test</button>
+        <div id="map"></div>
     </body>
-
+  </html>
